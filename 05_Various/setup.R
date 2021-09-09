bf5ac5376d7192e0b77ed99200d26af55311d601
mhesselbarth <mhk.hesselbarth@gmail.com>
2021-09-09 14:08:56 -0400

    Adding setup script and parameter script

---
+++ 05_Various/setup.R
@@ -0,0 +1,30 @@
+##--------------------------------------------##
+##    Author: Maximilian H.K. Hesselbarth     ##
+##    Coastal Ecology and Conservation Lab    ##
+##    University of Michigan                  ##
+##    mhessel@umich.edu                       ##
+##    www.github.com/mhesselbarth             ##
+##--------------------------------------------##
+
+# load packages #
+library(meta.arrR) # remotes::install_github("Allgeier-Lab/meta.arrR", ref = "development")
+library(arrR)
+
+library(suppoRt) # remotes::install_github("mhesselbarth/suppoRt")
+library(tidyverse)
+
+library(future)
+library(future.batchtools)
+library(future.apply)
+
+# Set some plotting defaults
+
+# DINA4
+units <- "mm"
+
+width <- 210
+
+height <- 297
+
+# set pixels
+dpi <- 900
