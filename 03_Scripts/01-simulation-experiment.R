b2596085aa15d3136432911f0315add184d39381
mhesselbarth <mhk.hesselbarth@gmail.com>
2022-12-06 11:23:24 -0500

    Upate simulation experiment df

--- 03_Scripts/01-simulation-experiment.R
+++ 03_Scripts/01-simulation-experiment.R
@@ -16,7 +16,7 @@ source("01_Functions/setup.R")
 
 pop_n <- c(0, 8, 16, 32, 64, 128)
 
-set.seed(42)
+set.seed(25)
 
 matrix_lhs <- lhs::improvedLHS(n = iterations, k = 2, dup = 2)
 
@@ -30,7 +30,10 @@ table(cut(matrix_lhs[, 1], breaks = seq(0.0, 1, 0.2)),
 experiment_df <- tibble::tibble(biotic = matrix_lhs[, 1], 
                                 abiotic = matrix_lhs[, 2]) |> 
   dplyr::slice(rep(x = 1:dplyr::n(), times = length(pop_n))) |> 
-  dplyr::mutate(pop_n = rep(x = pop_n, each = iterations))
+  dplyr::mutate(pop_n = rep(x = pop_n, each = iterations)) |> 
+  dplyr::mutate(biotic = dplyr::case_when(pop_n == 0 ~ 0, TRUE ~ biotic)) |> 
+  dplyr::bind_rows(data.frame(biotic = matrix_lhs[, 1], abiotic = rep(x =  0, times = iterations), 
+                              pop_n = rep(c(8, 16, 32, 64, 128), each = iterations)))
 
 nrow(experiment_df)
 
@@ -38,7 +41,7 @@ nrow(experiment_df)
 
 #### Check parameter space ####
 
-dplyr::filter(experiment_df, pop_n == 8) |> 
+dplyr::filter(experiment_df, pop_n == 8, abiotic != 0) |> 
   ggplot() + 
   geom_point(aes(x = biotic, y = abiotic)) + 
   geom_hline(yintercept = 0.0, color = "grey", linetype = 2) + geom_hline(yintercept = 1.0, color = "grey", linetype = 2) + 
---
+++ 03_Scripts/99-3-abiotic-biotic-only.R
@@ -0,0 +1,171 @@
+##--------------------------------------------##
+##    Author: Maximilian H.K. Hesselbarth     ##
+##    Coastal Ecology and Conservation Lab    ##
+##    University of Michigan                  ##
+##    mhessel@umich.edu                       ##
+##    www.github.com/mhesselbarth             ##
+##--------------------------------------------##
+
+# Purpose: Model runs with with variation (abiotic or biotic) only
+
+#### Load setup ####
+
+source("01_Functions/setup.R")
+source("01_Functions/import-cv.R")
+source("01_Functions/import-mortality.R")
+
+#### Load/wrangle simulated data ####
+
+results_phase_df <- import_cv(path = "02_Data/result-phase.rds")
+
+results_noise_df <- import_cv(path = "02_Data/result-noise.rds")
+
+results_combined_df <- dplyr::bind_rows(phase = results_phase_df, noise = results_noise_df, 
+                                        .id = "scenario") |> 
+  dplyr::mutate(scenario = factor(scenario, levels = c("phase", "noise")))
+
+#### Load mortality data ####
+
+mortality_phase_df <- import_mortality(path = "02_Data/result-phase.rds") |> 
+  dplyr::filter(pop_n > 0)
+
+mortality_noise_df <- import_mortality(path = "02_Data/result-noise.rds") |> 
+  dplyr::filter(pop_n > 0)
+
+mortality_combined_df <- dplyr::bind_rows(phase = mortality_phase_df, noise = mortality_noise_df,
+                                          .id = "scenario") |>
+  dplyr::mutate(scenario = factor(scenario, levels = c("phase", "noise"))) |>
+  dplyr::group_by(scenario, row_id, pop_n, nutrient_input) |>
+  dplyr::summarise(died_total = mean(died_total), .groups = "drop")
+
+#### Filter abundances/mortality #### 
+
+results_combined_df <- dplyr::left_join(x = results_combined_df, y = mortality_combined_df, 
+                                        by = c("scenario", "row_id", "pop_n", "nutrient_input")) |> 
+  dplyr::mutate(include = dplyr::case_when(pop_n == 128 & nutrient_input == "low" &
+                                             died_total> threshold_mort ~ "no", 
+                                           TRUE ~ "yes")) |> 
+  dplyr::filter(include == "yes")
+
+#### Abiotic variation only ####
+
+gg_abiotic <- purrr::map(c(phase = "phase", noise = "noise"), function(scenario_i) {
+  
+  purrr::map(c(ag_production = "ag_production", bg_production = "bg_production", ttl_production = "ttl_production"), function(part_i) {
+    
+      df_temp <- dplyr::filter(results_combined_df, scenario == scenario_i, 
+                               part == part_i, pop_n == 0, include == "yes") |> 
+        dplyr::select(row_id, pop_n, nutrient_input, measure, value.cv, abiotic, biotic)
+      
+      alpha_gamma_temp <- dplyr::filter(df_temp, measure %in% c("alpha", "gamma")) |> 
+        tidyr::pivot_wider(names_from = measure, values_from = value.cv)
+        
+      beta_temp <- dplyr::filter(df_temp, measure == "beta")
+      
+      gg_alpha_gamma <- ggplot(data = alpha_gamma_temp, aes(x = alpha, y = gamma)) + 
+        
+        geom_point(pch = 1, alpha = 0.5) + 
+        geom_abline(color = "grey", linetype = 2) +
+        geom_smooth(method = 'loess', formula = "y ~ x", se = FALSE, color = "#6ad5e8") + 
+        
+        facet_grid(rows = dplyr::vars(nutrient_input), scales = "fixed") + 
+        
+        labs(x = bquote(CV[Local~scale]), y = bquote(CV[Meta-ecosystem~scale])) +
+        theme_classic() + 
+        theme(strip.background = element_blank(), strip.text = element_blank(),
+              axis.line = element_blank(), panel.border = element_rect(linewidth = 0.5, fill = NA),
+              legend.position = "none")
+      
+      gg_beta <- ggplot(data = beta_temp, aes(x = abiotic, y = value.cv)) + 
+        
+        geom_point(pch = 1, alpha = 0.5) + 
+        geom_smooth(method = 'loess', formula = "y ~ x", se = FALSE, color = "#6ad5e8") + 
+        
+        facet_grid(rows = dplyr::vars(nutrient_input), scales = "fixed", 
+                   labeller = labeller(nutrient_input = function(x) paste0("Nutr. input: ", x))) + 
+        
+        labs(x = bquote(Variation[Nutrient~subsidies]), y = bquote(CV[beta])) +
+        theme_classic() + 
+        theme(strip.background = element_blank(), strip.text = element_text(hjust = 0.5),
+              axis.line = element_blank(), panel.border = element_rect(linewidth = 0.5, fill = NA),
+              legend.position = "none")
+      
+      cowplot::plot_grid(gg_alpha_gamma, gg_beta, ncol = 2)
+    
+  })
+})
+
+#### Biotic connectivity only ####
+
+gg_dummy <- ggplot(data = data.frame(pop_n = c(8, 16, 32, 64, 128),  x = c(1, 2, 3, 4, 5),
+                                     y = c(5, 4, 3, 2, 1)), 
+                   aes(x = x, y = y, color = factor(pop_n))) +
+  geom_point(size = 3.5) + 
+  scale_color_viridis_d(name = "Population size", option = "C") + 
+  theme_classic() +
+  theme(legend.position = "bottom")
+
+gg_biotic <- purrr::map(c(phase = "phase", noise = "noise"), function(scenario_i) {
+  
+  purrr::map(c(ag_production = "ag_production", bg_production = "bg_production", ttl_production = "ttl_production"), function(part_i) {
+    
+    df_temp <- dplyr::filter(results_combined_df, scenario == scenario_i, 
+                             part == part_i, abiotic == 0.0, include == "yes") |> 
+      dplyr::select(row_id, pop_n, nutrient_input, measure, value.cv, abiotic, biotic)
+    
+    alpha_gamma_temp <- dplyr::filter(df_temp, measure %in% c("alpha", "gamma")) |> 
+      tidyr::pivot_wider(names_from = measure, values_from = value.cv)
+    
+    beta_temp <- dplyr::filter(df_temp, measure == "beta")
+    
+    gg_alpha_gamma <- ggplot(data = alpha_gamma_temp, aes(x = alpha, y = gamma, color = pop_n)) + 
+      
+      geom_point(pch = 1, alpha = 0.5) + 
+      geom_abline(color = "grey", linetype = 2) +
+      geom_smooth(method = 'loess', formula = "y ~ x", se = FALSE) + 
+      
+      facet_grid(rows = dplyr::vars(nutrient_input), scales = "fixed") + 
+      
+      scale_color_viridis_d(option = "C") +
+      
+      labs(x = bquote(CV[Local~scale]), y = bquote(CV[Meta-ecosystem~scale])) +
+      theme_classic() + 
+      theme(strip.background = element_blank(), strip.text = element_blank(),
+            axis.line = element_blank(), panel.border = element_rect(linewidth = 0.5, fill = NA),
+            legend.position = "none")
+    
+    gg_beta <- ggplot(data = beta_temp, aes(x = biotic, y = value.cv, color = pop_n)) +
+      
+      geom_point(pch = 1, alpha = 0.5) + 
+      geom_smooth(method = 'loess', formula = "y ~ x", se = FALSE) + 
+      
+      facet_grid(rows = dplyr::vars(nutrient_input), scales = "fixed", 
+                 labeller = labeller(nutrient_input = function(x) paste0("Nutr. input: ", x))) + 
+      
+      scale_color_viridis_d(option = "C") +
+      
+      labs(x = "Connectivity", y = bquote(CV[beta])) +
+      theme_classic() + 
+      theme(strip.background = element_blank(), strip.text = element_text(hjust = 0.5),
+            axis.line = element_blank(), panel.border = element_rect(linewidth = 0.5, fill = NA),
+            legend.position = "none")
+    
+    gg_combined <- cowplot::plot_grid(gg_alpha_gamma, gg_beta, ncol = 2)
+    
+    cowplot::plot_grid(gg_combined, cowplot::get_legend(gg_dummy),
+                       nrow = 2, ncol = 1, rel_heights = c(0.95, 0.05))
+    
+  })
+})
+
+#### Save ggplot ####
+
+overwrite <- FALSE
+
+suppoRt::save_ggplot(plot = gg_abiotic$noise$ag_production, filename = "Figure-A3.pdf",
+                     path = "04_Figures/Appendix/", width = width, height = height * 0.5,
+                     units = units, dpi = dpi, overwrite = overwrite)
+
+suppoRt::save_ggplot(plot = gg_biotic$noise$ag_production, filename = "Figure-A4.pdf",
+                     path = "04_Figures/Appendix/", width = width, height = height * 0.5,
+                     units = units, dpi = dpi, overwrite = overwrite)
