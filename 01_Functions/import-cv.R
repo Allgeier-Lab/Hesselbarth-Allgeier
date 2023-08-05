0acf09198b57baf0eacb3bef758f076b6282727f
mhesselbarth <mhk.hesselbarth@gmail.com>
2023-08-05 15:54:24 +0200

    Import cv and mortality using unordered factors

--- 01_Functions/import-cv.R
+++ 01_Functions/import-cv.R
@@ -16,12 +16,16 @@ import_cv <- function(path, near = FALSE) {
                                      "biotic", "abiotic"))
         
       }
+    
+      dplyr::rename(x, "value.sd" = "sd", "value.mn" = "mean")
       
-      dplyr::rename(x, "value.sd" = "sd", "value.mn" = "mean")}, .id = "row_id") |> 
-    dplyr::mutate(row_id = as.numeric(row_id),part = factor(part, levels = c("ag_production", "bg_production", "ttl_production")),
+    }, .id = "row_id") |> 
+    dplyr::mutate(row_id = as.numeric(row_id), 
+                  part = factor(part, levels = c("ag_production", "bg_production", "ttl_production")),
                   measure = factor(measure, levels = c("alpha", "gamma", "beta", "synchrony")),
-                  pop_n = factor(as.numeric(pop_n), ordered = TRUE),
-                  nutrient_input = factor(nutrient_input, ordered = TRUE, labels = c("low", "medium", "high"))) |>
+                  pop_n = factor(as.numeric(pop_n), levels = c(0,8, 16, 32, 64, 128)), 
+                  nutrient_input = factor(nutrient_input, levels = c(6.18915377290985e-05, 0.000121005, 0.000191781), 
+                                          labels = c("low", "medium", "high"))) |>
     dplyr::select("row_id", "part", "measure", "pop_n", "nutrient_input",
                   "biotic", "abiotic","value.cv", "value.mn", "value.sd", "value.prod") |> 
     tibble::tibble()
--- 01_Functions/import-mortality.R
+++ 01_Functions/import-mortality.R
@@ -3,11 +3,12 @@ import_mortality <- function(path) {
   readr::read_rds(path) |> 
     purrr::map_dfr(function(j) {
       
-      dplyr::left_join(x = j$connectivity, y = j$mortality, by = c("id", "pop_n", "nutrient_input", 
-                                                                   "biotic", "abiotic"),
+      dplyr::left_join(x = j$connectivity, y = j$mortality, 
+                       by = c("id", "pop_n", "nutrient_input", "biotic", "abiotic"),
                        suffix = c(".connectivity", ".mortality"), multiple = "all")}, .id = "row_id") |> 
-    dplyr::mutate(row_id = as.numeric(row_id), pop_n = factor(as.numeric(pop_n), ordered = TRUE),
-                  nutrient_input = factor(nutrient_input, ordered = TRUE, labels = c("low", "medium", "high"))) |>
+    dplyr::mutate(row_id = as.numeric(row_id), pop_n = factor(as.numeric(pop_n), levels = c(0,8, 16, 32, 64, 128)), 
+                  nutrient_input = factor(nutrient_input, levels = c(6.18915377290985e-05, 0.000121005, 0.000191781), 
+                                          labels = c("low", "medium", "high"))) |>
     tibble::tibble()
   
 }
