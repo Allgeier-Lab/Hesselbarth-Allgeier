8c42b1cb73b0cf09f239591fca5dd06a013ccf8e
mhesselbarth <mhk.hesselbarth@gmail.com>
2023-11-17 11:24:45 +0100

    Adding null cv import

--- 01_Functions/import-cv.R
+++ 01_Functions/import-cv.R
@@ -31,3 +31,23 @@ import_cv <- function(path, near = FALSE) {
     tibble::tibble()
   
 }
+
+import_cv_null <- function(path, near = FALSE) {
+  
+  readr::read_rds(path) |> 
+    purrr::map_dfr(function(j) {
+      dplyr::left_join(x = j$cv, y = j$prod, suffix = c(".cv", ".prod"),
+                            by = c("part", "measure", "pop_n", "nutrient_input")) |> 
+        dplyr::rename("value.sd" = "sd", "value.mn" = "mean")
+    }, .id = "row_id") |> 
+    dplyr::mutate(row_id = as.numeric(row_id), 
+                  part = factor(part, levels = c("ag_production", "bg_production", "ttl_production")),
+                  measure = factor(measure, levels = c("alpha", "gamma", "beta", "synchrony")),
+                  pop_n = factor(as.numeric(pop_n), levels = c(0, 8, 16, 32, 64, 128)), 
+                  nutrient_input = factor(nutrient_input, levels = c(6.18915377290985e-05, 0.000121005, 0.000191781), 
+                                          labels = c("low", "medium", "high"))) |>
+    dplyr::select("row_id", "part", "measure", "pop_n", "nutrient_input",
+                  "value.cv", "value.mn", "value.sd", "value.prod") |> 
+    tibble::tibble()
+  
+}
--- 01_Functions/import-mortality.R
+++ 01_Functions/import-mortality.R
@@ -12,3 +12,15 @@ import_mortality <- function(path) {
     tibble::tibble()
   
 }
+
+import_mortality_null <- function(path) {
+  
+  readr::read_rds(path) |> 
+    purrr::map_dfr(function(j) {
+      dplyr::mutate(j$mortality, pop_n = factor(as.numeric(pop_n), levels = c(0,8, 16, 32, 64, 128)), 
+                    nutrient_input = factor(nutrient_input, levels = c(6.18915377290985e-05, 0.000121005, 0.000191781), 
+                                            labels = c("low", "medium", "high")))
+      }, .id = "row_id") |> 
+    dplyr:::mutate(row_id = as.numeric(row_id))
+  
+}
