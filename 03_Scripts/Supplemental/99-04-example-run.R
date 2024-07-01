50e7c51b949df5ac6842ee8e4f84859e37dbca3e
mhesselbarth <mhk.hesselbarth@gmail.com>
2024-07-01 11:33:32 +0200

    Adding biomass-dist plot

--- 03_Scripts/Supplemental/99-04-example-run.R
+++ 03_Scripts/Supplemental/99-04-example-run.R
@@ -11,6 +11,7 @@
 #### Load setup ####
 
 source("01_Functions/setup.R")
+base_size <- 12.5
 
 #### Stable values #### 
 
@@ -48,9 +49,41 @@ result_temp <- meta.arrR::run_simulation_meta(metasyst = metasyst_temp, paramete
   meta.arrR::filter_meta(filter = c((max_i / years) * years_filter, max_i),
                          reset = TRUE, verbose = FALSE)
 
-#### Create ggplot ####
+### Summarise distance #### 
 
-base_size <- 13.5
+biomass_dist_df <- purrr::map_dfr(result_temp$seafloor, function(i) {
+  dplyr::filter(i , timestep == max(timestep), reef == 0) |> 
+  dplyr::mutate(dist = sqrt(x ^ 2 + y ^ 2), 
+                dist_class = cut(dist, breaks = seq(from = 0, to = max(dist) + 5,
+                                                    by = 5), ordered_result = TRUE)) |> 
+  dplyr::group_by(dist_class) |> 
+  dplyr::summarise(ag = mean(ag_biomass), bg = mean(bg_biomass))}, .id = "id") |> 
+  tidyr::pivot_longer(-c(id, dist_class)) |> 
+  dplyr::mutate(id = factor(id, levels = paste0("Meta_", 1:5), labels = paste0("Local system ", 1:5)), 
+                name = factor(name, levels = c("ag", "bg"), labels = c("Aboveground", "Belowground")))
+
+# set colors 
+color_meta <- MetBrewer::met.brewer(name = "Java", n = n, type = "discrete")
+names(color_meta) <- levels(input_values_noise$meta) |> as.character()
+
+gg_dist <- ggplot(data = biomass_dist_df, aes(x = dist_class, y = value, color = id, group = id)) + 
+  
+  # adding geoms
+  geom_line() + 
+  geom_point() + 
+  
+  # facet wrap meta ecosystems
+  facet_wrap(. ~ name, scales = "free_y") + 
+  
+  # scale fill
+  scale_color_manual(name = "", values = color_meta) + 
+  
+  # general theme
+  labs(x = "Distance to artifical reef [m]", y = expression(paste("Seagrasss biomass [", gDW~m^-2, "]"))) +
+  theme_classic(base_size = base_size) +
+  theme(legend.position = "bottom", strip.background = element_blank(), strip.text = element_text(hjust = 0))
+
+#### Create ggplot ####
 
 gg_biomass <- purrr::map_dfr(result_temp$seafloor, function(i) dplyr::filter(i, timestep == max(timestep)), 
                .id = "meta") |> 
@@ -61,11 +94,11 @@ gg_biomass <- purrr::map_dfr(result_temp$seafloor, function(i) dplyr::filter(i, 
   # adding raster geom
   geom_raster(aes(x = x, y = y, fill = ag_biomass)) + 
   
-  # faceet wrap singlular meta ecosystems
+  # facet wrap meta ecosystems
   facet_wrap(. ~ meta, nrow = 2, ncol = 3) + 
   
   # scale fill
-  scale_fill_gradientn(name = expression(paste("AG biomass [", gDW~d^-1~m^-2, "]")),
+  scale_fill_gradientn(name = expression(paste("AG biomass [", gDW~m^-2, "]")),
                        colors = rev(MetBrewer::met.brewer(name = "Hokusai1", n = 255))) +
   coord_fixed() +
   
@@ -75,12 +108,16 @@ gg_biomass <- purrr::map_dfr(result_temp$seafloor, function(i) dplyr::filter(i, 
         axis.title = element_blank(), axis.ticks = element_blank(),
         strip.background = element_blank(), strip.text = element_text(hjust = 0)) 
 
+#### Combine plots #### 
+
+gg_total <- cowplot::plot_grid(gg_biomass, gg_dist, labels = c("A)", "B)"), ncol = 1)
+
 #### Save result ####
 
-suppoRt::save_ggplot(plot = gg_biomass, filename = "Figure-S4.png",
-                     path = "04_Figures/Supplemental/", width = width, height = height * 0.5,
-                     units = units, dpi = dpi, overwrite = FALSE)
+suppoRt::save_ggplot(plot = gg_total, filename = "Figure-S4.png",
+                     path = "04_Figures/Supplemental/", width = width, height = height * 0.75,
+                     units = units, dpi = dpi, overwrite = TRUE)
 
-suppoRt::save_ggplot(plot = gg_biomass, filename = "Figure-S4.pdf",
-                     path = "04_Figures/Supplemental/", width = width, height = height * 0.5,
-                     units = units, dpi = dpi, overwrite = FALSE)
+suppoRt::save_ggplot(plot = gg_total, filename = "Figure-S4.pdf",
+                     path = "04_Figures/Supplemental/", width = width, height = height * 0.75,
+                     units = units, dpi = dpi, overwrite = TRUE)
